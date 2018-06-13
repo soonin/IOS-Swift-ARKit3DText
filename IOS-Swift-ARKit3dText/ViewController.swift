f6bdac36473d4b73e6194954376e7d2b9806c6a8
Pooya Hatami <pooya@node-1w7jr9qrok5tbmfbapha9d96d.ipv6.telus.net>
2018-06-13 13:47:57 -0700

    ready!

--- IOS-Swift-ARKit3dText/ViewController.swift
+++ IOS-Swift-ARKit3dText/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var scnView: ARSCNView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scnView.delegate = self
+        scnView.add3DText(showText: self.inputText!.text! )
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        scnView.session.run(configuration)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        scnView.session.pause()
+    }
+    
 
+    @IBAction func updateAct(_ sender: Any) {
+        scnView.removeNodeByName(nodeName: "flyingText")
+        scnView.add3DText(showText: self.inputText!.text! ,nodeName: "flyingText")
+    }
+    
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    
 }
 
